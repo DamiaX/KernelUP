e41b5232689ba8c2fab3d975eec8a74818a3b0d6
Damian Majchrzak <DamiaX@users.noreply.github.com>
2014-05-10 14:49:06 +0200

    Update remove.sh

--- remove.sh
+++ remove.sh
@@ -25,6 +25,7 @@ sudo rm -rf /usr/share/icons/hicolor/128x128/apps/kernelup.png;
 sudo rm -rf ~/.config/autostart/kernelup-init.desktop; 
 sudo rm -rf /usr/local/sbin/kernelup*; 
 sudo rm -rf /usr/share/applications/kernelup-init.desktop;
+sudo rm -rf /var/log/kernelup.log;
 sudo rm -rf kernelup*; 
 rm -rf $temp;
 rm -rf $temp1;
