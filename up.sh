dcd2eecbc7f89a2bc802405849fdbf6ee637f95e
Damian Majchrzak <DamiaX@users.noreply.github.com>
2014-05-15 22:45:53 +0200

    Update up.sh

--- up.sh
+++ up.sh
@@ -17,9 +17,9 @@ fi
 
 sudo rm -rf /tmp/kernelup*; 
 sudo rm -rf /usr/share/icons/hicolor/128x128/apps/kernelup.png; 
-sudo rm -rf ~/.config/autostart/kernelup-init.desktop; 
+sudo rm -rf ~/.config/autostart/kernelup*;
 sudo rm -rf /usr/local/sbin/kernelup*; 
-sudo rm -rf /usr/share/applications/kernelup-init.desktop;
+sudo rm -rf /usr/share/applications/kernelup*;
 sudo rm -rf kernelup*;
 
 rm -rf $temp;
