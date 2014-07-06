ed5dd8ca08e5cf0a30a7f9f74e86f04fd98d73a4
Damian Majchrzak <DamiaX@users.noreply.github.com>
2014-07-06 17:37:58 +0200

    Create remove.sh

---
+++ Core/remove.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#Copyright © 2014 Damian Majchrzak (DamiaX)
+#http://damiax.github.io/kernelup/
+
+temp=".temp";
+
+ps -e | grep 'kernelup-init' >$temp
+
+if [ -s $temp ]
+ then
+killall 'sleep'; 
+killall kernelup-init;
+fi
+
+sudo rm -rf /tmp/kernelup*; 
+sudo rm -rf /usr/share/icons/hicolor/128x128/apps/kernelup.png; 
+sudo rm -rf ~/.config/autostart/kernelup*;
+sudo rm -rf /usr/local/sbin/kernelup*; 
+sudo rm -rf /usr/share/applications/kernelup*;
+sudo rm -rf $HOME/.KernelUP_data;
+sudo rm -rf kernelup*; 
+rm -rf $temp;
+
+sudo rm -rf "remove.sh"
+
+exit;
