84b45e1301da871cbb0a8584cf460f6d0411233a
Damian Majchrzak <DamiaX@users.noreply.github.com>
2014-10-20 22:50:30 +0200

    Update kernelup.sh

--- kernelup.sh
+++ kernelup.sh
@@ -3,7 +3,7 @@
 #Copyright © 2014 Damian Majchrzak (DamiaX)
 #http://damiax.github.io/kernelup/
 
-version="5.7";
+version="5.8";
 app='kernelup';
 version_url="https://raw.githubusercontent.com/DamiaX/kernelup/master/VERSION";
 ubuntu_url="http://kernel.ubuntu.com/~kernel-ppa/mainline";
