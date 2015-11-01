55d6c03fcc443904bce6c3aa366c8c77c1d35867
Damian Majchrzak <DamiaX@users.noreply.github.com>
2015-11-01 01:36:52 +0100

    Create kernelup_show.c

---
+++ Core/kernelup_show.c
@@ -0,0 +1,21 @@
+//Copyright © 2015 Damian Majchrzak (DamiaX)
+//http://damiax.github.io/kernelup/
+
+#include <stdio.h>
+
+void unhide(char *text)                       
+{
+  int iPrzes = 0, iOile[3] = { 3, 1, 2 };
+  for (int i=0; text[i] != 0; i++)
+  {
+    text[i] = ( text[i] - iOile[iPrzes] );
+    iPrzes ++;
+    if(iPrzes == 3) iPrzes = 0;
+  }
+  
+} 
+int main(int argc, char *argv[])
+{
+  unhide(argv[1]);
+  printf (argv[1]);
+ }
